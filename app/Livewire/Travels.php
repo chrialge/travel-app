f676a9858d314cdd6ab12ad1008395fd6a9dd69b
chrialge <chrialge@gmail.com>
2024-12-11 09:40:13 +0100

    add fic

--- app/Livewire/Travels.php
+++ app/Livewire/Travels.php
@@ -15,15 +15,17 @@ class Travels extends Component
 
     public function render()
     {
-        // variabile che salva l'id dell'utente che si e loggato
-        $id = Auth::id();
+        // // variabile che salva l'id dell'utente che si e loggato
+        // $id = Auth::id();
 
-        // variabile con tutti i viaggi nel db in ordine discendente in base chi si e loggato
-        $travels = Travel::where(function ($query) {
-            $query->where('name', 'LIKE', '%' . $this->search . '%')
-                ->orWhere('date_start', 'LIKE', '%' . $this->search . '%')
-                ->orWhere('date_finish', 'LIKE', '%' . $this->search . '%');
-        })->where('user_id', $id)->orderByDesc('id')->paginate(5);
+        // // variabile con tutti i viaggi nel db in ordine discendente in base chi si e loggato
+        // $travels = Travel::where(function ($query) {
+        //     $query->where('name', 'LIKE', '%' . $this->search . '%')
+        //         ->orWhere('date_start', 'LIKE', '%' . $this->search . '%')
+        //         ->orWhere('date_finish', 'LIKE', '%' . $this->search . '%');
+        // })->where('user_id', $id)->orderByDesc('id')->paginate(5);
+
+        $travels = Travel::orderByDesc('id')->paginate(5);
 
         return view('livewire.travels', compact('travels'));
     }
